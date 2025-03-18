04e1cf49b0096919098461f01020b8a6f49e3568
Brandon Allen <brandon.allen@yale.edu>
2025-03-18 15:41:41 -0400

    Made module names lower-case, renamed classes in closed_systems

--- src/qflux/__init__.py
+++ src/qflux/__init__.py
@@ -2,5 +2,5 @@ from qflux.utils.logging_config import setup_logging
 
 setup_logging()
 
-from Closed_Systems import *
-from Open_Systems import *
+from closed_systems import *
+from open_systems import *
